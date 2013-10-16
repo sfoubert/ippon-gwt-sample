963647963229921c750fd24b650b5c89669fd2f2
Sebastien Foubert <sebastien.foubert@gmail.com>
2013-10-16 18:01:46 +0200

    Add Spring GWT RPC

--- src/main/java/fr/ippon/gwt/server/GreetingServiceImpl.java
+++ src/main/java/fr/ippon/gwt/server/GreetingServiceImpl.java
@@ -10,29 +10,30 @@ import fr.ippon.gwt.shared.FieldVerifier;
 /**
  * The server side implementation of the RPC service.
  */
-@SuppressWarnings("serial")
 @Service("greetService")
 public class GreetingServiceImpl extends RemoteServiceServlet implements
     GreetingService {
 
-  public String greetServer(String input) throws IllegalArgumentException {
-    // Verify that the input is valid.
-    if (!FieldVerifier.isValidName(input)) {
-      // If the input is not valid, throw an IllegalArgumentException back to
-      // the client.
-      throw new IllegalArgumentException(
-          "Name must be at least 4 characters long");
-    }
+	private static final long serialVersionUID = 588551943990379565L;
 
-    String serverInfo = getServletContext().getServerInfo();
-    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
-
-    // Escape data from the client to avoid cross-site script vulnerabilities.
-    input = escapeHtml(input);
-    userAgent = escapeHtml(userAgent);
-
-    return "Hello, " + input + "!<br><br>I am running " + serverInfo
-        + ".<br><br>It looks like you are using:<br>" + userAgent;
+	public String greetServer(String input) throws IllegalArgumentException {
+	    // Verify that the input is valid.
+	    if (!FieldVerifier.isValidName(input)) {
+	      // If the input is not valid, throw an IllegalArgumentException back to
+	      // the client.
+	      throw new IllegalArgumentException(
+	          "Name must be at least 4 characters long");
+	    }
+	
+	    String serverInfo = getServletContext().getServerInfo();
+	    String userAgent = getThreadLocalRequest().getHeader("User-Agent");
+	
+	    // Escape data from the client to avoid cross-site script vulnerabilities.
+	    input = escapeHtml(input);
+	    userAgent = escapeHtml(userAgent);
+	
+	    return "Hello, " + input + "!<br><br>I am running " + serverInfo
+	        + ".<br><br>It looks like you are using:<br>" + userAgent;
   }
 
   /**
